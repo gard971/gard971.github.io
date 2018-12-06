12a8237afd1474ac4f363a0bf9138ffee5386d29
gard971 <32454699+gard971@users.noreply.github.com>
2018-12-06 10:28:34 +0100

    Update b4mk.js

--- b4mk.js
+++ b4mk.js
@@ -1,5 +1,3 @@
-
-      
 (function(){
 var pass = localStorage.getItem("password")
 	enter4();
@@ -61,7 +59,7 @@ window.location.href="index.html"
     enter(); 
 		function enter(){
     
-    if(localStorage.getItem "hack" == undefined){
+    if(localStorage.getItem("hack") == undefined){
 
       localStorage.setItem("hack", "0")
     }
@@ -112,7 +110,7 @@ function enter2(){
           window.location.href="hacker.html"
         }
 
-       else if(form.userid.value == "admin" && form.pswrd.value == "28112003"){
+       else if(form.userid.value == "admin" && (form.pswrd.value) == "28112003"){
 
        		
                
