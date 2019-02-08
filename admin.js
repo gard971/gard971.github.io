f6a45db76e79a4aa8edd64b6340383dff5a70f56
gard971 <32454699+gard971@users.noreply.github.com>
2019-02-08 15:00:30 +0100

    Update admin.js

--- admin.js
+++ admin.js
@@ -16,14 +16,18 @@ window.location.href="pass.html"
 		function log(){ 
                 
 		        var b = localStorage.getItem("password") 
-		       
-			if(localStorage.getItem("password") == "true" || sessionStorage.getItem("password") == "true" || localStorage.getItem("password") == "true" && sessionStorage.getItem("password") == "true"){
+		        var pass1 = localStorage.getItem("password")
+			var pass2 = SessionStorage.getItem("password")
+			if(pass1 == "true" || pass2 == "true" || pass1 == "true" && pass2 == "true"){
 			}
                    
                         
-                        else{
+                        else if(pass1 == undefined && pass2 == undefined){
+				
+				
 				window.location.href="pass.html"
 			}
+	else{
                          
                         window.location.href="pass.html"
 
